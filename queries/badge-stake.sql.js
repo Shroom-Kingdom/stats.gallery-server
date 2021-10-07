df4c8ea72bd7a761ac7492e48aed99ca77f981b7
Mario Reder <mreder1289@gmail.com>
2021-10-07 13:54:10 +0200

    improve stake and score query

--- queries/badge-stake.sql.js
+++ queries/badge-stake.sql.js
@@ -2,30 +2,17 @@ const { sql } = require('slonik');
 
 module.exports = params => {
   return sql`
-    select count(*) as result
-    where exists(
-      select * from (
-        select receiver_account_id from (
-          select produced_receipt_id from (
-            select converted_into_receipt_id
-            from transactions
-            where signer_account_id = ${params.account_id}
-          ) r
-          left join execution_outcome_receipts on executed_receipt_id = converted_into_receipt_id
-        ) re
-        inner join execution_outcome_receipts on executed_receipt_id = re.produced_receipt_id
-        left join receipts on execution_outcome_receipts.produced_receipt_id = receipts.receipt_id
-        union
-        select receiver_account_id from (
-          select converted_into_receipt_id
-          from transactions
-          where signer_account_id = ${params.account_id}
-        ) r
-        left join execution_outcome_receipts on executed_receipt_id = converted_into_receipt_id
-        left join receipts on produced_receipt_id = receipts.receipt_id
-      ) a
-      where receiver_account_id like '%.poolv1.near'
-      or receiver_account_id like 'meta-pool.near'
-    )
+    select
+    case
+      when exists (
+        select *
+        from receipts
+        join action_receipt_actions ara on receipts.receipt_id = ara.receipt_id
+        where predecessor_account_id = ${params.account_id}
+          and (receiver_account_id like '%.poolv1.near' or receiver_account_id = 'meta-pool.near')
+          and args->>'method_name' = 'deposit_and_stake'
+      ) then 1
+      else 0
+    end as result
   `;
 };
